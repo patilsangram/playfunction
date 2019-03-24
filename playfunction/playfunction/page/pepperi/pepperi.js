fe0f2fb6ed4c248a707fceb956b9dc41f4700a7a
Sangram Patil <sangram.p@indictranstech.com>
2019-03-24 22:50:08 +0530

    [pepperi-page] added proper layout, menu page changes,css

--- playfunction/playfunction/page/pepperi/pepperi.js
+++ playfunction/playfunction/page/pepperi/pepperi.js
@@ -7,11 +7,23 @@ frappe.pages['pepperi'].on_page_load = function(wrapper) {
 frappe.pepperi = Class.extend({
 	init: function(opts) {
 		this.$wrapper = opts.$wrapper
-		this.render_layout();
+		this.make();
 	},
 
-	render_layout: function() {
+	make: function() {
 		this.$wrapper.empty();
-		this.$wrapper.append(frappe.render_template("pepperi_home"));
+		this.$wrapper.append(frappe.render_template("pepperi_layout"));
+		this.$header = this.$wrapper.find(".pepperi-header");
+		this.$main = this.$wrapper.find(".pepperi-main");
+		this.$sidebar = this.$wrapper.find(".pepperi-sidebar");
+		this.$content = this.$wrapper.find(".pepperi-content");
+		this.home();
+	},
+
+	home: function() {
+		this.$header.empty()
+		this.$main.empty()
+		this.$header.append(frappe.render_template("pep_header"))
+		this.$main.append(frappe.render_template("pepperi_home"))
 	}
 })
