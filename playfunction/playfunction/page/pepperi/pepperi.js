b49511a8f6aa8eddd37d3e2556f314508d67827f
Sangram Patil <sangram.p@indictranstech.com>
2019-04-07 20:43:51 +0530

    [pepperi] seperate item grid template. Item grid triggers

--- playfunction/playfunction/page/pepperi/pepperi.js
+++ playfunction/playfunction/page/pepperi/pepperi.js
@@ -25,6 +25,9 @@ frappe.pepperi = Class.extend({
 		this.$header.append(frappe.render_template("pep_header"))
 		this.$main.append(frappe.render_template("pepperi_home"))
 		this.bind_events();
+
+		localStorage.removeItem("items");
+		localStorage.removeItem("filters");
 	},
 
 	bind_events: function() {
@@ -105,8 +108,89 @@ frappe.pepperi = Class.extend({
 			callback: function(r) {
 				me.$main.empty()
 				me.$main.append(frappe.render_template("pepperi_item_list", {
-					"data": r.message, "item_group": filters["item_group"]}))
+					"data": r.message, "item_group": filters["item_group"], "total": 0}))
+				$('.pepperi-content').html(frappe.render_template('scope_items', {"data": r.message}))
+				me.search_item();
+				me.show_item_details();
+				me.image_view();
+				me.unit_qty_change();
 			}
 		})
+	},
+
+	search_item: function() {
+		var me = this;
+		$('.search-btn').click(function() {
+			var search_txt = $('.search_ip').val();
+			if(search_txt) {
+				frappe.msgprint(search_txt)
+			}
+		})
+
+		$('.search-clr').click(function() {
+			var search_txt = $('.search_ip').val();
+			if(search_txt) {
+				$('.search_ip').val("");
+			}
+		})
+	},
+
+	show_item_details: function() {
+		var me = this;
+		$('.ObjectMenu').click(function() {
+			let item_code = $(this).attr("data-item")
+			frappe.msgprint(item_code)
+		})
+	},
+
+	image_view: function() {
+		var me = this;
+		$('.img-view').click(function() {
+			let image =  $(this).attr("data-img")
+			frappe.msgprint(image)
+		})
+	},
+
+	unit_qty_change: function() {
+		var me = this;
+		// decrease-qty
+		$('.qty-minus').click(function() {
+			var qty = $(this).closest("div.gQs").find("input[name='UnitsQty']").val();
+			if (qty && parseInt(qty) > 0) {
+				var item_code = $(this).closest("div.gQs").attr("data-item");
+				qty = parseInt(qty) - 1
+				$(this).closest("div.gQs").find("input[name='UnitsQty']").val(qty);
+				me.update_cart(item_code, qty);
+			}
+		})
+		// increase-qty
+		$('.qty-plus').click(function() {
+			var qty = $(this).closest("div.gQs").find("input[name='UnitsQty']").val();
+			qty = parseInt(qty) + 1
+			var item_code = $(this).closest("div.gQs").attr("data-item");
+			$(this).closest("div.gQs").find("input[name='UnitsQty']").val(qty);
+
+			me.update_cart(item_code, qty);
+		})
+		// qty-change
+		$('.unitqty').change(function() {
+			var qty = parseInt($(this).val()) || 0;
+			var item_code = $(this).closest("div.gQs").attr("data-item");
+			$(this).closest("div.gQs").find("input[name='UnitsQty']").val(qty);
+			me.update_cart(item_code, qty)
+		})
+	},
+
+	update_cart: function(item_code, qty) {
+		var items = JSON.parse(localStorage.getItem('items')) || {};
+		if (qty == 0) {
+			delete items[item_code]
+		}
+		else {
+			items[item_code] = qty
+		}
+		localStorage.setItem('items', JSON.stringify(items));
+		console.log(JSON.parse(localStorage.getItem('items')))
+		//frappe.msgprint(repl("%(item)s - %(qty)s", {"item": item_code, "qty": qty}))
 	}
 })
