cee30d7d06be66b62a7c522a1b1db5f3acc16ace
Sangram Patil <sangram.p@indictranstech.com>
2019-05-01 23:45:02 +0530

    [Customer Discount % on pepperi & Quotation]

--- playfunction/playfunction/custom_script/quotation/quotation.py
+++ playfunction/playfunction/custom_script/quotation/quotation.py
@@ -39,12 +39,15 @@ def update_selling_data(doc,meathod):
 		update cost_price & discount_percentage & calculate selling_price i.e. Rate
 		formula: rate = cost_price * selling_rate
 	"""
+	cust_discount_per = 0
+	if doc.customer:
+		cust_discount_per = frappe.db.get_value("Customer", doc.customer, "discount_percentage")
 	for row in doc.items:
 		fields = ["last_purchase_rate as cost_price","discount_percentage"]
 		item_data = frappe.get_value("Item", row.item_code, fields, as_dict=True)
 		if not row.cost_price and item_data.get("cost_price"):
 			row.cost_price = item_data.get("cost_price")
-		if not row.discount_percentage and item_data.get("discount_percentage"):
-			row.discount_percentage = item_data.get("discount_percentage")
+		if not row.discount_percentage and (cust_discount_per or item_data.get("discount_percentage")):
+			row.discount_percentage = cust_discount_per or item_data.get("discount_percentage")
 		if row.cost_price and row.selling_rate:
 			row.rate = row.cost_price * row.selling_rate
