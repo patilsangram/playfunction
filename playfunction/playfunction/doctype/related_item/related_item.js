8b94e1882313dc5209916344b77c5cab34d1ac51
Sangram Patil <sangram.p@indictranstech.com>
2019-05-21 09:38:18 +0530

    [fix] Related Item Import

--- playfunction/playfunction/doctype/related_item/related_item.js
+++ playfunction/playfunction/doctype/related_item/related_item.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2019, Indictrans and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Related Items', {
+frappe.ui.form.on('Related Item', {
 	refresh: function(frm) {
 
 	}
--- playfunction/playfunction/doctype/related_item/test_related_item.py
+++ playfunction/playfunction/doctype/related_item/test_related_item.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 import frappe
 import unittest
 
-class TestRelatedItems(unittest.TestCase):
+class TestRelatedItem(unittest.TestCase):
 	pass
