e77e4e38b7c36f1d794a7a338a9ba9d959f55fce
yugandhara <yugandhara.b@indictranstech.com>
2019-05-31 17:07:40 +0530

    [changes group_level field]

--- playfunction/playfunction/custom_script/item_group/item_group.py
+++ playfunction/playfunction/custom_script/item_group/item_group.py
@@ -4,7 +4,7 @@ from frappe import _
 @frappe.whitelist()
 def update_group_level(doc, method=None):
 	if doc.parent_item_group:
-		parent_level = frappe.db.get_value("Item Group", doc.parent_item_group, "group_level") or 1
+		parent_level = frappe.db.get_value("Item Group", doc.parent_item_group, "group_level")
 		if parent_level <= 3:
 			doc.group_level = parent_level + 1
 		else:
