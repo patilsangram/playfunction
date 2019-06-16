f092b4a0f50ea0bc5c331cf78b47cfc66c0d1ac6
Sangram Patil <sangram.p@indictranstech.com>
2019-06-17 02:06:22 +0530

    [Sales Order Approval flow]

--- playfunction/playfunction/custom_script/sales_order/sales_order.js
+++ playfunction/playfunction/custom_script/sales_order/sales_order.js
@@ -2,5 +2,38 @@ frappe.ui.form.on("Sales Order",{
 	refresh: function(frm) {
 		// from public - playfunction_selling.js
 		playfunction.selling.set_field_permissions();
+		if (frm.doc.docstatus == 0 && frm.doc.workflow_state != "Rejected") {
+			frm.events.init_approval_flow(frm);
+		}
+	},
+
+	init_approval_flow: function(frm){
+		// TODO - Make commomn workflow for Quotation & SO
+		if(frappe.user.has_role("PlayFunction Admin")) {
+			frm.add_custom_button("Approve", function() {
+				frm.trigger('approve_order');
+			}, "Action")
+
+			frm.add_custom_button("Reject", function() {
+				frm.trigger('reject_order');
+			}, "Action")
+			frm.page.set_inner_btn_group_as_primary(__("Action"));
+		}
+	},
+
+	approve_order:function(frm){
+		frm.doc.workflow_state = "Approved";
+		frm.savesubmit();
+	},
+
+	reject_order:function(frm){
+		frm.doc.workflow_state = "Rejected";
+		frm.save();
+	},
+
+	before_submit:function(frm){
+		if(frm.doc.workflow_state != "Approved"){
+			frappe.throw(__("Sales Order must be approved by Admin"))
+		}
 	}
 })
