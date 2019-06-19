9eb98adc139cb1a69d592f9d12b7f3305b0b3ec2
yugandhara <yugandhara.b@indictranstech.com>
2019-06-19 17:37:43 +0530

    item doc changes

--- playfunction/playfunction/custom_script/item/item.js
+++ playfunction/playfunction/custom_script/item/item.js
@@ -21,7 +21,36 @@ frappe.ui.form.on("Item", {
 			frappe.throw(__("Duplicate Entry found in Related Items."))
 		}
 	},
+	is_stock_item:function(frm){
+	    if(frm.doc.is_stock_item==1){
+			cur_frm.set_df_property("catalogs", "reqd", frm.doc.is_stock_item=="1");
+	    }
+	    else{
+			cur_frm.set_df_property("catalogs", "reqd", false);
+	    }
+	},
+	onload:function(frm){
+		cur_frm.set_df_property("item_group", "hidden", true);
+		cur_frm.set_df_property("allow_alternative_item", "hidden", true);
+	},
+	quick_entry:function(frm){
+		cur_frm.set_df_property("item_group", "hidden", true);
+	},
+	sp_without_vat:function(frm){
+		if(frm.doc.sp_without_vat>0){
+			var sp_with_vat=frm.doc.sp_without_vat*0.17;
+			cur_frm.set_value("sp_with_vat",sp_with_vat);
+		}
+	},
+
 });
+
+
+cur_frm.fields_dict['related_item'].grid.get_field('item_code').get_query =function(frm,cdt,cdn){
+	return {
+		"filters": {'group_level': 1}
+	}
+}
 
 cur_frm.fields_dict['catalogs'].grid.get_field('catalog_level_1').get_query =function(frm,cdt,cdn){
 	return {
