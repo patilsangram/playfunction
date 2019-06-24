f7fa8d5324e58157ec5b8bb3c4e8d35f2f6a466c
stripti <tripti.s@indictrans.in>
2019-06-24 14:39:07 +0530

    user_api

--- playfunction/playfunction/mobile_api/user.py
+++ playfunction/playfunction/mobile_api/user.py
@@ -1,7 +1,6 @@
 import frappe
 import requests
 import json
-from frappe.utils import validate_email_add
 from frappe import _
 
 
@@ -10,40 +9,70 @@ def ping():
 	return "Pong"
 
 
-
+#LOGIN API
 @frappe.whitelist(allow_guest=True)
-def login(data):
-	""":param data: {
-			"usr": "yugandhara.b@indictranstech.com",
-			"pwd": "admin@123"
-		}
+def login(usr,pwd):
 	"""
-	user_data = json.loads(data)
+		:param usr: username or email_id
+		:param pwd: password
+	"""
+	
 	try:
-		if user_data.get("usr") and user_data.get("pwd"):
-			if user:
-				frappe.clear_cache(user = user)
-				frappe.local.login_manager.authenticate(user,user_data["pwd"])
+		response = frappe._dict({})
+		if usr and pwd:
+		
+				frappe.local.login_manager.authenticate(usr,pwd)
 				frappe.local.login_manager.post_login()
 				frappe.response["sid"] = frappe.session.sid
 				frappe.response["message"] = "Logged In"
-				mobile_no = frappe.db.get_value('User',{"name":user_data.get("usr")},'mobile_no')
-				if mobile_no:
-					frappe.response["mobile_no"] = mobile_no
 				frappe.response["status_code"] = 200
-
-			else:
-				frappe.response["message"] = "Invalid Email Id or Mobile no."
-				frappe.response["status_code"] = 404
 		else:
-			raise frappe.throw(_("Invalid Input"))
+				frappe.response["message"] = "Invalid Email Id"
+				frappe.response["status_code"] = 404
+		
 	except frappe.AuthenticationError,e:
 		http_status_code = getattr(e, "http_status_code", 500)
 		frappe.response["status_code"] = http_status_code
 
-	
+
+#FORGOT_PASSWORD API
+@frappe.whitelist(allow_guest=True)
+def forgot_password(usr):
+	if usr=="Administrator":
+		return 'not allowed'
+
+	try:
+		usr = frappe.get_doc("User", usr)
+		if not usr.enabled:
+			return 'disabled'
+
+		usr.validate_reset_password()
+		usr.reset_password(send_email=True)
+
+		return frappe.msgprint(_("Password reset instructions have been sent to your email"))
+
+	except frappe.DoesNotExistError:
+		frappe.clear_messages()
+		return 'not found'
+
+
+
+#LOGOUT API
+@frappe.whitelist(allow_guest=True)
+def logout(usr,pwd):
+		frappe.local.login_manager.logout()
+		frappe.db.commit()
+		frappe.response["message"] = "Logged out"
 
 
 
 
-	
+
+
+
+
+
+
+		
+		
+
