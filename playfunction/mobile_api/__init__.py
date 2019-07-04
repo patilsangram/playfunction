0d9b5c0b3c9b2995c0dfb6dae671b63d09879dd0
Sangram Patil <sangram.p@indictranstech.com>
2019-07-04 08:26:05 +0530

    [Customer- Update customer, search API]

--- playfunction/mobile_api/__init__.py
+++ playfunction/mobile_api/__init__.py
@@ -3,4 +3,5 @@ import json
 
 from .user import ping, login, forgot_password, update_password
 from .category import get_categories
-from .customer import get_customer_list, delete_customer, create_customer, get_customer_details
+from .customer import get_customer_list, delete_customer, create_customer, \
+	get_customer_details, update_customer
