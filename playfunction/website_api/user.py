a4c4a3b680be22dfdbe6a4e76ec884ecc976597f
stripti <tripti.s@indictrans.in>
2019-07-31 20:17:59 +0530

    [website_api][forgot & update password]

--- playfunction/website_api/user.py
+++ playfunction/website_api/user.py
@@ -66,3 +66,80 @@ def logout(usr):
 		response["message"] = "Unable to Logout."
 		frappe.local.response["http_status_code"] = http_status_code
 		frappe.log_error(message=frappe.get_traceback() , title="Website API: logout")
+
+@frappe.whitelist(allow_guest=True)
+def forgot_password(data):
+	""":param data: {
+			"usr": "test@gmail.com"
+		}
+	"""
+	try:
+		from frappe.utils import random_string, get_url
+		args = json.loads(data)
+		response = frappe._dict({})
+		user = frappe.db.exists("User", args.get("usr"))
+		if user :
+			user = frappe.get_doc("User", user)
+			key = random_string(32)
+			user.db_set("reset_password_key", key)
+			link = get_url("/update-password?key=" + key)
+			user.password_reset_mail(link)
+			response.key = key
+			response.message = _("Password reset instructions have been sent to your email")
+			response.status_code = 200
+			frappe.local.response['http_status_code'] = 200
+		else:
+			response.message = _("Invalid User")
+			response.status_code = 404
+			frappe.local.response['http_status_code'] = 404
+	except Exception, e:
+		response["message"] = "Forgot Password failed"
+		http_status_code = getattr(e, "http_status_code", 500)
+		response.status_code = http_status_code
+		frappe.local.response["http_status_code"] = http_status_code
+		frappe.log_error(message=frappe.get_traceback() , title="Website API: forgot_password")
+	finally:message
+		return response
+
+@frappe.whitelist(allow_guest=True)
+def update_password(data):
+	""":param data: {
+			"usr": "test@gmail.com",
+			"key": "AR2xs49BkdNPUmBnfjHCou6QAOxx7wFj",
+			"new_password": "test@1234",
+			"old_password": "test@12"
+		}
+	"""
+	from frappe.utils.password import update_password as _update_password
+	try:
+		args = json.loads(data)
+		response = frappe._dict({})
+		user = frappe.db.exists("User", args.get("usr"))
+		if user:
+			if args.get("key"):
+				user = frappe.db.get_value("User", {"reset_password_key":args.get("key")})
+				if not user:
+					response.message = _("Password reset key is expired")
+					response.status_code = 417
+					frappe.local.response['http_status_code'] = 417
+					return response
+			elif args.get("old_password"):
+				frappe.local.login_manager.check_password(user, args.get("old_password"))
+			
+			_update_password(user, args.get("new_password"))
+			frappe.db.set_value("User", user, "reset_password_key", "")
+			response.message = _("Password reset sucessfully")
+			response.status_code = 200
+			frappe.local.response['http_status_code'] = 200
+		else:
+			response.message = _("Invalid User")
+			response.status_code = 404
+			frappe.local.response['http_status_code'] = 404
+	except Exception, e:
+		http_status_code = getattr(e, "http_status_code", 500)
+		response.status_code = http_status_code
+		response["message"] = "Update Password failed"
+		frappe.local.response["http_status_code"] = http_status_code
+		frappe.log_error(message=frappe.get_traceback() , title="Website API: update_password")
+	finally:
+		return response
