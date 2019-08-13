2aede57db327caeb4b972f1a19dc5ded20bce9a3
stripti <tripti.s@indictrans.in>
2019-08-13 19:02:13 +0530

    [website_api][wishlist][toyfinder]

--- playfunction/website_api/item.py
+++ playfunction/website_api/item.py
@@ -116,7 +116,153 @@ def search(search=None):
 		response["status_code"] = http_status_code
 		frappe.local.response["http_status_code"] = http_status_code
 		response["message"] = "Unable to fetch details: {}".format(str(e))
-		frappe.log_error(message=frappe.get_traceback() , title="Website API: search")
+		frappe.log_error(message=frappe.get_traceback() , title = "Website API: search")
 	finally:
 		return response
 
+@frappe.whitelist()
+def add_to_wishlist(data):
+	"""
+	 data:{ "item_code":
+		  "item_name":
+		  "qty":
+		 "wishlist_date":
+		  "age":
+		  "uom":
+		  }
+	"""
+	try:
+		response = frappe._dict()
+		items = json.loads(data)
+		cust = frappe.db.get_value("Customer",{'user':frappe.session.user},"name")
+		if cust:
+			wishlist_doc = frappe.get_value("Wishlist",{'customer':cust},"name")
+			if wishlist_doc:
+				wishlist_doc = frappe.get_doc("Wishlist",wishlist_doc)
+				existing_item = False
+				for row in wishlist_doc.get("items"):
+					# update item row
+					if row.get("item_code") == items.get("item_code"):
+						existing_item = True
+						row.update(items)
+				# add new item
+				if not existing_item:
+					wishlist_doc.append("items", items)
+				wishlist_doc.save(ignore_permissions= True)
+				frappe.db.commit()
+				response["message"] = "Wishlist is updated"
+				response["status_code"] = 200
+				frappe.local.response["http_status_code"] = 200
+			else:
+				wishlist_doc = frappe.new_doc("Wishlist")
+				wishlist_doc.customer = cust
+				wishlist_doc.user = frappe.session.user 
+				wishlist_doc.append("items", items)
+				wishlist_doc.save(ignore_permissions= True)
+				frappe.db.commit()
+				response["message"] = "Wishlist Created"
+				response["status_code"] = 200
+				frappe.local.response["http_status_code"] = 200
+		else:
+			response["status_code"] = 404
+			response["message"] = "Customer not found"
+			frappe.local.response['http_status_code'] = 404
+
+	except Exception as e:
+		http_status_code = getattr(e, "http_status_code", 500)
+		response["status_code"] = http_status_code
+		frappe.local.response['http_status_code'] = http_status_code
+		response["message"] = "Unable to create Wishlist: {}".format(str(e))
+		frappe.log_error(message=frappe.get_traceback() , title = "Website API: add_to_wishlist")
+	finally:
+		return response
+
+@frappe.whitelist()
+def delete_wishlist(name,item_code):
+	try:
+		response = frappe._dict()
+		item_code = item_code.encode('utf-8')
+		item_list= [ i.strip() for i in item_code.split(",")]
+		cust = frappe.db.get_value("Customer",{'user':frappe.session.user},"name")
+		wish_doc = frappe.get_value("Wishlist",{'customer':cust},"name")
+		if frappe.db.exists("Wishlist",wish_doc):
+			wish = frappe.get_doc("Wishlist", wish_doc)
+			new_items = []
+			for idx, row in enumerate(wish.get("items")):
+				if not row.item_code in item_list:
+					new_items.append(row)
+			wish.items = new_items
+			wish.flags.ignore_mandatory = True
+			wish.save()
+
+
+			if not len(wish.get("items", [])):
+				frappe.delete_doc("Wishlist", wish_doc)
+				response["message"] = "Deleted all items"
+				frappe.local.response["http_status_code"] = 200
+			frappe.db.commit()
+		else:
+			response["status_code"] = 404
+			response["message"] = "Wishlist not found"
+			frappe.local.response['http_status_code'] = 404
+	except Exception as e:
+		http_status_code = getattr(e, "http_status_code", 500)
+		response["status_code"] = http_status_code
+		frappe.local.response['http_status_code'] = http_status_code
+		response["message"] = "Unable to delete Wishlist"
+		frappe.log_error(message=frappe.get_traceback() , title = "Website API: delete_wishlist")
+	finally:
+		return response
+
+@frappe.whitelist()
+def get_wishlist_details():
+	try:
+		response = frappe._dict()
+		cust = frappe.db.get_value("Customer",{'user':frappe.session.user},"name")
+		if cust:
+			wishlist = frappe.db.sql(""" select w.customer, wi.item_code, wi.item_name, wi.age, wi.qty, wi.rate from `tabWishlist` w left join `tabWishlist Item` wi on w.name = wi.parent and w.customer = '{0}' """.format(cust),as_dict=1)
+			response["Wishlist Details"] = wishlist
+	except Exception as e:
+		http_status_code = getattr(e, "http_status_code", 500)
+		frappe.local.response['http_status_code'] = http_status_code
+		response["message"] = "Unable to fetch wishlist"
+		frappe.log_error(message = frappe.get_traceback() , title = "Mobile API: get_wishlist_details")
+	finally:
+		return response
+
+@frappe.whitelist()
+def toyfinder(category, age=None, subcategory=None):
+	try:
+		response = frappe._dict()
+		cond = " where 1=1"
+		category = category if not subcategory else subcategory
+		if category :
+			child_cat = get_child_categories(category)
+			child_cat.append(category)
+			chid_category = "(" + ",".join("'{}'".format(c) for c in child_cat) + ")"
+			cond += " and (c.catalog_level_1 in {0} or c.catalog_level_2 in {0})".format(chid_category)
+
+		if age:
+			cond += " and (i.age like '{0}')".format(age)
+
+		query = """
+			select distinct
+				i.name, i.item_name, i.age as age_range, i.sp_without_vat as selling_price
+			from
+				`tabItem` i left join `tabBin` b on b.item_code = i.name
+			left join
+				`tabCatalog` c on c.parent = i.name
+			{} 	group by i.name
+		""".format(cond)
+		items = frappe.db.sql(query,as_dict=True)
+		response["items"] = items
+		response["status_code"] = 200
+		frappe.local.response["http_status_code"] = 200
+	except Exception as e:
+		http_status_code = getattr(e, "http_status_code", 500)
+		response["status_code"] = http_status_code
+		frappe.local.response["http_status_code"] = http_status_code
+		response["message"] = "Unable to fetch toys: {}".format(str(e))
+		frappe.log_error(message=frappe.get_traceback() , title = "Website API: toyfinder")
+	finally:
+		return response
