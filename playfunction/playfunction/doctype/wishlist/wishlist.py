15c616214f3aa87d567bea1fa57d3b60b6afff68
stripti <tripti.s@indictrans.in>
2019-08-26 16:06:32 +0530

    [website_api][fix][allow_guest][wishlist_amount_fetched]

--- playfunction/playfunction/doctype/wishlist/wishlist.py
+++ playfunction/playfunction/doctype/wishlist/wishlist.py
@@ -5,6 +5,12 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 class Wishlist(Document):
-	pass
+	def validate(self):
+		update_amount(self)
+
+def update_amount(self):
+	for i in self.get("items"):
+		i.amount = flt(i.qty) * flt(i.rate)
