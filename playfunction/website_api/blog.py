acf57cf09f5039768a6a7a3a0aa86af54f2aac73
Sangram Patil <sangram.p@indictranstech.com>
2019-09-03 13:35:08 +0530

    [Project, Blog, Expert] doctype fixes, pagination in APIs

--- playfunction/website_api/blog.py
+++ playfunction/website_api/blog.py
@@ -3,42 +3,40 @@ import json
 from frappe import _
 
 
+fields = ["name", "title", "content", "blog_category", "image"]
+
 @frappe.whitelist(allow_guest=True)
-def get_blog_list():
-	"""Returns blog List"""
+def get_blog_list(page_index=0, page_size=10):
+	"""Returns blog post List"""
 	try:
 		response = frappe._dict()
-		fields = ["name", "title", "content", "blog_category", "image"]
-		blog_list = frappe.get_all('Blog Post',fields = fields, limit=None, start=None)
-		response.update({"data":blog_list})
+		blog_list = frappe.get_all("Blog Post", fields=fields, start=page_index,\
+			limit=page_size, order_by="creation")
+		response.update({"data": blog_list})
 	except Exception as e:
 		http_status_code = getattr(e, "http_status_code", 500)
 		frappe.local.response['http_status_code'] = http_status_code
 		response["message"] = "Unable to fetch blog list".format(str(e))
-		frappe.log_error(message = frappe.get_traceback() , title = "website API: get_blog_list")
+		frappe.log_error(message=frappe.get_traceback() , title="website API: get_blog_list")
 	finally:
 		return response
 
 
 @frappe.whitelist(allow_guest=True)
 def get_blog(blog_id):
+	"""Return single Blog Details"""
 	try:
 		response = frappe._dict()
 		if frappe.db.exists("Blog Post", blog_id):
-			doc = frappe.get_doc("Blog Post", blog_id)
-			response["blog_id"] = doc.name
-			response["title"] = doc.title
-			response["content"] = doc.content
-			response["blog_category"] = doc.blog_category
-			response["image"] = doc.image
+			blog_data = frappe.db.get_value("Blog Post", blog_id, fields, as_dict=True)
+			response["data"] = blog_data
 		else:
-			response["status_code"] = 404
 			frappe.local.response['http_status_code'] = 404
+			response["message"] = "Blog Not found"
 	except Exception as e:
 		http_status_code = getattr(e, "http_status_code", 500)
-		response["status_code"] = http_status_code
-		frappe.local.response['http_status_code'] = http_status_code
+		frappe.local.response["http_status_code"] = http_status_code
 		response["message"] = "Unable to fetch blog Details: {}".format(str(e))
-		frappe.log_error(message = frappe.get_traceback() , title = "website API: get_blog")
+		frappe.log_error(message=frappe.get_traceback(), title="website API: get_blog")
 	finally:
 		return response
--- playfunction/website_api/projects.py
+++ playfunction/website_api/projects.py
@@ -3,17 +3,19 @@ import json
 from frappe import _
 
 
+fields = ["name","project_name", "title", "description", "image"]
+
 @frappe.whitelist(allow_guest=True)
-def get_project_list():
+def get_project_list(page_index=0, page_size=10):
 	"""Returns project List"""
 	try:
 		response = frappe._dict()
-		fields = ["project_name", "title", "description", "image"]
-		project_list = frappe.get_all('Playfunction Project',fields = fields, limit=None, start=None)
-		response.update({"data":project_list})
+		project_list = frappe.get_all("Playfunction Project", fields=fields, start=page_index,
+			limit=page_size, order_by="creation")
+		response.update({"data": project_list})
 	except Exception as e:
 		http_status_code = getattr(e, "http_status_code", 500)
-		frappe.local.response['http_status_code'] = http_status_code
+		frappe.local.response["http_status_code"] = http_status_code
 		response["message"] = "Unable to fetch order list"
 		frappe.log_error(message = frappe.get_traceback() , title = "Website API: get_project_list")
 	finally:
@@ -24,20 +26,15 @@ def get_project_details(project_id):
 	try:
 		response = frappe._dict()
 		if frappe.db.exists("Playfunction Project", project_id):
-			doc = frappe.get_doc("Playfunction Project", project_id)
-			response["project_id"] = doc.project_name
-			response["title"] = doc.title
-			response["description"] = doc.description
-			response["image"] = doc.image
+			project_data = frappe.db.get_value("Playfunction Project", project_id, fields, as_dict=True)
+			response["data"] = project_data
 		else:
-			response["status_code"] = 404
-			response["message"] = "Projects not found"
-			frappe.local.response['http_status_code'] = 404
+			response["message"] = "Given Project ID not found"
+			frappe.local.response["http_status_code"] = 404
 	except Exception as e:
 		http_status_code = getattr(e, "http_status_code", 500)
-		response["status_code"] = http_status_code
-		frappe.local.response['http_status_code'] = http_status_code
-		response["message"] = "Unable to fetch project Details: {}".format(str(e))
+		frappe.local.response["http_status_code"] = http_status_code
+		response["message"] = "Unable to fetch Project Details: {}".format(str(e))
 		frappe.log_error(message = frappe.get_traceback() , title = "Website API: get_project_details")
 	finally:
 		return response
