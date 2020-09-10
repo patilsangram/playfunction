989238878e83fb8e9cdc1c046958266efe0abbde
pratik.m <pratik.m@indictrans.in>
2020-09-10 11:48:39 +0530

    [fix][item][custom_script][item group was hidden, made it visible]

--- playfunction/playfunction/custom_script/delivery_note/delivery_note.py
+++ playfunction/playfunction/custom_script/delivery_note/delivery_note.py
@@ -7,10 +7,12 @@ def submit(doc, method):
     for i in receipient.recipients:
         cc.append(i.cc)
     sales_invoice = doc.items
+    si_name = sales_invoice[0].against_sales_invoice
+    order_date = frappe.db.get_value("Sales Invoice",{"name":si_name},"posting_date")
     frappe.sendmail(
                     recipients = frappe.db.get_value("Customer",{"name":doc.customer},"user"),
                     cc = cc,
                     subject = receipient.subject,
-                    message = frappe.render_template(receipient.message,{"doc":doc,"si":sales_invoice[0].against_sales_invoice})
+                    message = frappe.render_template(receipient.message,{"doc":doc,"si":si_name,"date":order_date})
                     # attachments= print_att
                     )
