dffab7e9c4539e858934ee21c490a77f5ac43cb4
pratik.m <pratik.m@indictrans.in>
2020-10-19 16:09:58 +0530

    [fix][Email][Attachment added]

--- playfunction/playfunction/custom_script/sales_invoice/sales_invoice.py
+++ playfunction/playfunction/custom_script/sales_invoice/sales_invoice.py
@@ -5,6 +5,8 @@ def submit(doc, method):
     receipient = frappe.get_doc("Notification","Sales Invoice")
     cc = []
     # subject = receipient.subject.format(doc.name)
+    print_doc = frappe.get_print('Sales Invoice', doc.name, doc = None, print_format = receipient.print_format,as_pdf=1)
+    print_att = [{'fname':doc.name +".pdf",'fcontent':print_doc}]
     for i in receipient.recipients:
         cc.append(i.cc)
     frappe.sendmail(
@@ -12,6 +14,6 @@ def submit(doc, method):
                     recipients = frappe.db.get_value("Customer",{"name":doc.customer},"user"),
                     cc = cc,
                     subject = receipient.subject,
-                    message = frappe.render_template(receipient.message,{"doc":doc})
-                    # attachments= print_att
+                    message = frappe.render_template(receipient.message,{"doc":doc}),
+                    attachments= print_att
                     )
--- playfunction/playfunction/custom_script/sales_order/sales_order.py
+++ playfunction/playfunction/custom_script/sales_order/sales_order.py
@@ -4,12 +4,15 @@ import frappe
 def submit(doc, method):
     receipient = frappe.get_doc("Notification","Sales Order")
     cc = []
+    print_doc = frappe.get_print('Sales Order', doc.name, doc = None, print_format = receipient.print_format,as_pdf=1)
+    print_att = [{'fname':doc.name +".pdf",'fcontent':print_doc}]
+
     for i in receipient.recipients:
         cc.append(i.cc)
     frappe.sendmail(
                     recipients = frappe.db.get_value("Customer",{"name":doc.customer},"user"),
                     cc = cc,
                     subject = receipient.subject,
-                    message = receipient.message
-                    # attachments= print_att
+                    message = receipient.message,
+                    attachments= print_att
                     )
