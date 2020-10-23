39dff8eff9c8516b7264c4ecfef16e2eb2f340e3
pratik.m <pratik.m@indictrans.in>
2020-10-23 13:12:13 +0530

    [fix][page][pepperi][print formats and emails are done]

--- playfunction/playfunction/custom_script/sales_order/sales_order.py
+++ playfunction/playfunction/custom_script/sales_order/sales_order.py
@@ -5,13 +5,14 @@ def submit(doc, method):
     try:
         receipient = frappe.get_doc("Notification","Sales Order")
         cc = []
+        party_name = doc.customer if doc.customer != "" else ""
         print_doc = frappe.get_print('Sales Order', doc.name, doc = None, print_format = receipient.print_format,as_pdf=1)
         print_att = [{'fname':doc.name +".pdf",'fcontent':print_doc}]
         for i in receipient.recipients:
             cc.append(i.cc)
         frappe.sendmail(
         # recipients = "pratik.m@indictrans.in",
-        recipients = frappe.db.get_value("Customer",{"name":doc.customer},"user"),
+        recipients = frappe.db.get_value("Customer",{"name":party_name},"user"),
         cc = cc,
         subject = receipient.subject,
         message = frappe.render_template(receipient.message,{"doc":doc}),
