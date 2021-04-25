15b777e298f99e062d239343c02f24dfb23ec039
Sangram Patil <sangram.p@indictranstech.com>
2021-04-25 13:23:40 +0530

    [SI email send fix]

--- playfunction/playfunction/custom_script/sales_invoice/sales_invoice.py
+++ playfunction/playfunction/custom_script/sales_invoice/sales_invoice.py
@@ -2,18 +2,23 @@ import frappe
 
 
 def submit(doc, method):
-    receipient = frappe.get_doc("Notification","Sales Invoice")
-    cc = []
-    # subject = receipient.subject.format(doc.name)
-    print_doc = frappe.get_print('Sales Invoice', doc.name, doc = None, print_format = receipient.print_format,as_pdf=1)
-    print_att = [{'fname':doc.name +".pdf",'fcontent':print_doc}]
-    for i in receipient.recipients:
-        cc.append(i.cc)
-    frappe.sendmail(
-                    # recipients = "pratik.m@indictrans.in",
-                    recipients = frappe.db.get_value("Customer",{"name":doc.customer},"user"),
-                    cc = cc,
-                    subject = receipient.subject,
-                    message = frappe.render_template(receipient.message,{"doc":doc}),
-                    attachments= print_att
-                    )
+    try:
+        if frappe.session.user == "Guest":
+          frappe.set_user("Administrator")
+        receipient = frappe.get_doc("Notification","Sales Invoice")
+        cc = []
+        # subject = receipient.subject.format(doc.name)
+        print_doc = frappe.get_print('Sales Invoice', doc.name, doc = None, print_format = receipient.print_format,as_pdf=1)
+        print_att = [{'fname':doc.name +".pdf",'fcontent':print_doc}]
+        for i in receipient.recipients:
+            cc.append(i.cc)
+        frappe.sendmail(
+            # recipients = "pratik.m@indictrans.in",
+            recipients = frappe.db.get_value("Customer",{"name":doc.customer},"user"),
+            cc = cc,
+            subject = receipient.subject,
+            message = frappe.render_template(receipient.message,{"doc":doc}),
+            attachments= print_att
+            )
+    except Exception as e:
+        pass
