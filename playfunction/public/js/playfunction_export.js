6e19725c8abc7c35d4da2f889a598666c868b688
Sangram Patil <sangram.p@indictranstech.com>
2021-06-10 12:02:19 +0530

    [export featur for draft]

--- playfunction/public/js/playfunction_export.js
+++ playfunction/public/js/playfunction_export.js
@@ -3,7 +3,7 @@ frappe.provide("playfunction.export");
 playfunction.export.export_excel = function(doc) {
 
 	var doctype = cur_frm.doc.doctype
-	if(has_common(["Quotation", "Sales Order"], [doctype]) && cur_frm.has_perm("submit"), cur_frm.doc.docstatus == 1) {
+	if(has_common(["Quotation", "Sales Order"], [doctype]) && cur_frm.has_perm("submit") && cur_frm.doc.docstatus != 2) {
 		cur_frm.add_custom_button(__("Export"), function() {
 			let get_template_url = '/api/method/playfunction.playfunction.playfunction_export.export_data';
 			open_url_post(get_template_url, {dt: cur_frm.doc.doctype, dn: cur_frm.doc.name});
